68d99c57d8ee472802b378c4a216265b13df72fb
unknown <timothy.christian10@gmail.com>
2024-06-01 12:03:45 +0100

    removed commented codes

--- src/components/ParallelQueries.page.js
+++ src/components/ParallelQueries.page.js
@@ -1,17 +1,17 @@
-// import { useQuery } from 'react-query'
-// import axios from 'axios'
+import { useQuery } from 'react-query'
+import axios from 'axios'
 
-// const fetchSuperHeroes = () => {
-//   return axios.get('http://localhost:4000/superheroes')
-// }
+const fetchSuperHeroes = () => {
+  return axios.get('http://localhost:4000/superheroes')
+}
 
-// const fetchFriends = () => {
-//   return axios.get('http://localhost:4000/friends')
-// }
+const fetchFriends = () => {
+  return axios.get('http://localhost:4000/friends')
+}
 
-// export const ParallelQueriesPage = () => {
-//   const { data: superHeroes } = useQuery('super-heroes', fetchSuperHeroes)
-//   const { data: friends } = useQuery('friends', fetchFriends)
-//   console.log(superHeroes, friends)
-//   return <div>Parallel Queries</div>
-// }
+export const ParallelQueriesPage = () => {
+  const { data: superHeroes } = useQuery('super-heroes', fetchSuperHeroes)
+  const { data: friends } = useQuery('friends', fetchFriends)
+  console.log(superHeroes, friends)
+  return <div>Parallel Queries</div>
+}
